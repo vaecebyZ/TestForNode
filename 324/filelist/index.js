5af9a9ff14d6a3502f50283e881b2f5e18a23d6a
vaecebyz <vaeceby2@qq.com>
2021-03-24 17:44:47 +0800

    实现文件类型大小使用第三方模块处理时间

--- 324/filelist/index.js
+++ 324/filelist/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const http = require('http')
+const moment = require('moment')
+
 const server = http.createServer()
 
 
@@ -10,15 +12,47 @@ server.listen(80, err => {
 
 server.on('request', (req, rsp) => {
     console.log(req.url);
-    if (req.method == 'GET' && req.url == '/') {//处理首页访问
+    if (req.method == 'GET' && req.url == '/') { //处理首页访问
         fs.readFile('list.html', 'utf-8', (err, data) => {
             if (err) throw err
             rsp.end(data)
         })
-    } else if (req.method=='POST'&&req.url == '/files') { //注意请求格式
-        fs.readdir('./', 'utf8', (err, data) => {
+    } else if (req.method == 'POST' && req.url == '/files') { //注意请求格式
+
+        fs.readdir('./', 'utf8', (err, datas) => {
+
             if (err) throw err
-            rsp.end(JSON.stringify(data))
+
+            arrfile = [];
+
+            datas.forEach((element, index) => { //遍历文件名
+
+                fs.stat(element, (err, data) => { //读取文件详情
+
+                    file = {};
+
+                    if (err) throw err
+
+                    //对象数据
+                    //判断是否为文件
+                    if (data.isFile()) {
+                        file.fileType = '-'
+                    } else {
+                        file.fileType = 'd'
+                    }
+                    file.name = element;
+
+                    file.addTime = moment(data.birthtime).format('YYYY-MM-DD hh:mm:ss')//使用第三方库格式化时间
+                    file.size = (data.size / 1024).toFixed(2) + 'k'
+                    arrfile.push(file)
+
+                    if (datas.length == ++index) {
+                        console.log(arrfile);
+                        rsp.end(JSON.stringify(arrfile))
+                    }
+
+                })
+            });
         })
     } else { //处理静态文件 不要加utf8
         fs.readFile('.' + req.url, (err, data) => {
