8f3de99ed361c9a81ae48cb058e0b34f1c03390c
vaecebyz <vaeceby2@qq.com>
2021-03-26 22:11:17 +0800

    用户展示完成

--- 326_mysql/js/doData.js
+++ 326_mysql/js/doData.js
@@ -6,30 +6,34 @@ const moment = require('moment')
 template.defaults.root = './'
 
 module.exports = (req, res) => {
-    
+
     if (req.method == 'GET' && req.url == '/') {
 
-        conn.connect('select * from users',()=>{
-           // console.log(conn.result);
+        conn.connect('select * from users', () => {
+            // console.log(conn.result);
             let result = conn.result
-            
-            if(result.err == null){
 
-                result.data.forEach(e=>{
+            if (result.err == null) {
+
+                result.data.forEach(e => {
                     e.date = moment(e.date).format('YYYY-MM-DD hh:mm:ss')
                 })
 
                 let htmls = template('./viwes/index.html', {
-                    value: JSON.stringify(result.data)
+                    value: result.data
                 })
-        
+
                 res.end(htmls)
             }
 
         })
- 
-      
 
+
+    } else if (req.method == 'POST' && req.url == "/add") {
+        console.log(req);
+
+    } else if (req.method == 'POST' && req.url == "/search") {
+        console.log(req);
     } else {
         fs.readFile('.' + req.url, (err, data) => {
             if (err) throw err
