abc591bcb129272f35fe7abf0b8fc43556207dbb
vaecebyz <vaeceby2@qq.com>
2021-03-27 23:26:33 +0800

    修改新增

--- 326_mysql/js/doData.js
+++ 326_mysql/js/doData.js
@@ -2,19 +2,20 @@ const fs = require('fs')
 const conn = require('./conn')
 const template = require('art-template')
 const moment = require('moment')
+const queryString = require('querystring') //处理中文编码
 
 template.defaults.root = './'
 
 module.exports = (req, res) => {
 
-    let url = new URL('http://localhost'+req.url)
+    let url = new URL('http://localhost' + req.url)
 
     let urls = url.pathname
 
 
     if (req.method == 'GET') {
 
-        if ( urls == '/') {
+        if (urls == '/') {
 
             conn.connect('select * from users', () => {
                 // console.log(conn.result);
@@ -34,7 +35,7 @@ module.exports = (req, res) => {
                 }
             })
 
-        } else if ( urls == "/add") {
+        } else if (urls == "/add") {
 
             let htmls = template('./viwes/edit.html', {
                 value: '{}'
@@ -44,12 +45,12 @@ module.exports = (req, res) => {
 
             //console.log(url);
 
-        }else if ( urls == "/edit") {
-            
-           let userId =  url.searchParams.get('id') 
-           
-            if(userId!=null){
-                conn.connect('select * from users where userId = '+userId,()=>{
+        } else if (urls == "/edit") {
+
+            let userId = url.searchParams.get('id')
+
+            if (userId != null) {
+                conn.connect('select * from users where userId = ' + userId, () => {
                     let result = conn.result
 
                     if (result.err == null) {
@@ -57,18 +58,16 @@ module.exports = (req, res) => {
                         result.data.forEach(e => {
                             e.date = moment(e.date).format('YYYY-MM-DD hh:mm:ss')
                         })
-    
+
                         let htmls = template('./viwes/edit.html', {
-                            value: result.data
+                            value: result.data[0]
                         })
-    
+
                         res.end(htmls)
                     }
                 })
-               // console.log(url.searchParams.get('id'));
             }
-           // console.log(url);
-        } else if ( urls== "/search") {
+        } else if (urls == "/search") {
             console.log(url);
         } else {
             fs.readFile('.' + urls, (err, data) => {
@@ -77,13 +76,67 @@ module.exports = (req, res) => {
             })
         }
 
-    }else if (req.method =='POST'){
-        if(urls=="/edit"){
-           console.log(req);
-           res.end(JSON.stringify('edit post'))
+    } else if (req.method == 'POST') {
+        if (urls == "/edit") {
+
+            let data = ''
+
+            //获取Post数据 绑定data事件
+            req.on('data', che => {
+                data += che
+
+            })
+            //结束数据获取
+            req.on('end', () => {
+                let user = queryString.parse(data)
+                if (user.userId != '') {
+
+                    let sql =
+                        `update users set userName = "` + user.userName +
+                        `",userNickName="` + user.userNickName +
+                        `",userGender="` + user.userGender +
+                        `",userEmail="` + user.userEmail +
+                        `",userAddress="` + user.userAddress +
+                        `",userAbout="` + user.userAbout +
+                        `",isBan="` + user.isBan + `
+                        "where userId=` + user.userId
+
+                    conn.connect(sql, () => {
+                        let result = conn.result
+                        if (result.err == null) {
+                            let htmls = template('./viwes/edit.html', {
+                                value: user
+                            })
+                            res.end(htmls)
+                        }
+                    });
+                } else {
+
+                    let sql =
+                        `insert into users(userName,userNickName,userGender,userEmail,userAddress,userAbout,isBan,date ) values( "` + user.userName +
+                        `","` + user.userNickName +
+                        `","` + user.userGender +
+                        `","` + user.userEmail +
+                        `","` + user.userAddress +
+                        `","` + user.userAbout +
+                        `","` + user.isBan + 
+                        `","` + moment().format('YYYY-MM-DD hh:mm:ss') +
+                        `")`
+
+                        conn.connect(sql, () => {
+                            let result = conn.result
+                            if (result.err == null) {
+                                let htmls = template('./viwes/edit.html', {
+                                    value: user
+                                })
+                                res.end(htmls)
+                            }
+                        });
+
+                }
+            })
         }
-       
-    }else{
+    } else {
         res.end(JSON.stringify('下次一定'))
     }
 
